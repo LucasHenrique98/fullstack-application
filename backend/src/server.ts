e05a216a4018930907082e269a4e2870a624e0fe
LucasHenrique98 <henrique.lucas30@gmail.com>
2023-12-22 10:02:11 -0300

    feat: configur express server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,11 @@
-console.log('oi')
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen('8181', () => {
+  console.log(`Server is running on port 8181`);
+});
